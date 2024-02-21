605434e9b3ca5e258fe8d8ac3f240b8dccf02bf6
sescarrega <s.jescarrega@gmail.com>
2024-02-20 16:27:33 -0800

    are all the lements even

---
+++ evenElements.js
@@ -0,0 +1,16 @@
+//  given an arr write a function that returns true if all elements are even, otherwise false
+
+const evenElemnts = (arr)=>{
+    // initialize loop 
+    for(let i=0; i<arr.length; i++){
+        let index = arr[i];
+        if (index % 2 !==0 ){
+            return false;
+        }
+    }
+    return true; 
+}
+console.log(evenElemnts([2,4,6,11,12]));
+console.log(evenElemnts([12,240,67,11,12]));
+console.log(evenElemnts([12,240,68,10,12]));
+console.log(evenElemnts([2,2,68,10,12]));
